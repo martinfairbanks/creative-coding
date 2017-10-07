ec4976aed5cd4818ccadbb4bc37f9689ced6b09e
martinfairbanks <martinfbanks@gmail.com>
2017-10-07 02:26:01 +0200

    rose pattern

---
+++ effects/rhodonea_curve02.cpp
@@ -0,0 +1,36 @@
+﻿	/*	Rhodonea curve (rose) plotted in polar coordinates.
+		See https://en.wikipedia.org/wiki/Rose_(mathematics)
+		by Martin Fairbanks
+	*/
+	#define SDL2
+	#include "..\framework\creativeframework.cpp"
+	real32 angle = 0;
+	real32 k = 1;
+
+void setup()
+{
+	screen(1920, 1080, true, "rhodonea curve 2");
+	noStroke();
+	colorMode(HSB);
+}
+
+void updateAndDraw(uint32 t)
+{
+	clear(150, 255, 155);
+	int32 r, x, y;;
+	
+	k += 0.008;
+	for (real32 angle = 0; angle < TWO_PI; angle += 0.01)
+	{
+		//radius from center
+		r = windowWidth / 4 * cos(k*angle);
+		x = r * cos(angle);
+		y = r * sin(angle);
+		fill(random(255), 255, 255);
+		circle(center.x + x, center.y + y, 30);
+	}
+
+	uploadPixels();
+}
+
+void shutdown() { }
